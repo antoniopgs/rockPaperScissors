6b0ca6b1c4a624f9dee7c79bb3652a254651ab63
António <apgoncalves@outlook.pt>
2020-06-28 11:03:38 +0100

    Add stuff

--- rps.sol
+++ rps.sol
@@ -9,7 +9,7 @@ contract rps {
     }
     
     Player player1 = Player(1, 0xbDd5804F8eC5D4862C403aF6281caE11FC21f695, 1);
-    Player player2 = Player(2, 0x559Dbd861E393B359a55821FAc4b9eB75f42A337, 1);
+    Player player2 = Player(2, 0x559Dbd861E393B359a55821FAc4b9eB75f42A337, 2);
     
     function placeBet() external payable {}
     
@@ -18,7 +18,12 @@ contract rps {
     }
     
     function play() external payable returns(string memory) {
-        if (player1.choice == player2.choice) {return "Tie";}
+        
+        // If Tie:
+        if (player1.choice == player2.choice) {
+            player1.addr.transfer(address(this).balance / 2);
+            player2.addr.transfer(address(this).balance / 2);
+        }
         
         if (player1.choice == 1 && player2.choice == 2) {player2.addr.transfer(address(this).balance);}
         if (player1.choice == 1 && player2.choice == 3) {player1.addr.transfer(address(this).balance);}
@@ -29,10 +34,9 @@ contract rps {
         if (player1.choice == 3 && player2.choice == 1) {player2.addr.transfer(address(this).balance);}
         if (player1.choice == 3 && player2.choice == 2) {player1.addr.transfer(address(this).balance);}
         
-        else {return "Invalid input";}
+        else {
+            player1.addr.transfer(address(this).balance / 2);
+            player2.addr.transfer(address(this).balance / 2);
+        }
     }
-    
-    function sendEther(address payable _winner) internal {
-        _winner.transfer(address(this).balance);
-    } 
 }
