c504d6bafab7db590132110908a83ed8924512a7
António Gonçalves <44982443+antoniopgs@users.noreply.github.com>
2021-02-21 18:30:32 +0000

    Update constructor and more

--- rps.sol
+++ rps.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.10 <0.7.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 // Implements a Rock/Paper/Scissors game secured by a Commit-Reveal scheme
 contract RockPaperScissors {
     
-    address payable constant private admin = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    address payable constant private admin = payable(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
     
     uint private bet;
     
@@ -16,73 +16,101 @@ contract RockPaperScissors {
     
     enum Moves {NONE, ROCK, PAPER, SCISSORS}
     
-    enum Status {NONE, COMMITED, REVEALED}
-    
     struct Player {
         address payable addr;
         bytes32 commit;
         Moves move;
-        Status status;
     }
     
     Player[2] private players;
     
-    /*
-        Contract deployer will be set as Player 1
-        Move format examples: rock123, paperabc, scissorsxyz999
-        Don't forget to prepend your hash with "0x" if needed
-    */
-    constructor(uint commitPhaseSeconds, uint _revealPhaseSeconds, bytes32 p1MoveHash) public payable {
+    //Contract deployer will be set as Player 1
+    constructor(uint commitPhaseSeconds, uint _revealPhaseSeconds) {
+        
+        // Validate Phase Durations
         require(commitPhaseSeconds >= 60 && commitPhaseSeconds <= 300, "Commit Phase must last between 60 and 300 seconds.");
         require(_revealPhaseSeconds >= 60 && _revealPhaseSeconds <= 300, "Reveal Phase must last between 60 and 300 seconds.");
         
-        assert(players[0].addr == address(0) && players[1].addr == address(0)); // There must be no Preexisting players
-        assert(players[0].status != Status.COMMITED && players[0].status != Status.REVEALED); // Player 1 must have no status
-        require(msg.value > 0, "Player 1 must bet a positive amount");
-        bet = msg.value; // Set bet Player 2 will need to match
-        players[0] = Player(msg.sender, p1MoveHash, Moves.NONE, Status.COMMITED); // Add Player 1
-        revealPhaseSeconds = _revealPhaseSeconds; // Set Reveal Phase Duration - will be used in player2Commit()
-        commitDeadline = now + commitPhaseSeconds; // Set Commit Deadline
+        // Set Player 1 as Contract Deployer
+        players[0].addr = payable(msg.sender);
+        
+        // Set Reveal Phase Duration - will be used in commit()
+        revealPhaseSeconds = _revealPhaseSeconds;
+        
+        // Set Commit Deadline
+        commitDeadline = block.timestamp + commitPhaseSeconds;
     }
     
     /*
-        Caller of this function will be set as Player 2
         Move format examples: rock123, paperabc, scissorsxyz999
         Don't forget to prepend your hash with "0x" if needed
     */
-    function player2Commit(bytes32 p2MoveHash) external payable {
-        require(now < commitDeadline, "Commit Phase is over");
-        assert(players[0].addr != address(0)); // There must be a preexisting Player 1
-        assert(players[0].status == Status.COMMITED); // Player 1 must have commited.
-        require(msg.sender != players[0].addr, "Player 1 can't play against himself.");
-        require(players[1].addr == address(0), "There must be no preexisting Player 2");
-        require(msg.value == bet, "Player 2 must match Player 1's bet");
-        require(players[1].status != Status.COMMITED && players[1].status != Status.REVEALED, "You already commited");
-        players[1] = Player(msg.sender, p2MoveHash, Moves.NONE, Status.COMMITED); // Add Player 2
+    function commit(bytes32 moveHash) external payable {
         
-        // As soon as Player 2 commits, start reveal Phase
-        revealDeadline = now + revealPhaseSeconds;
+        // Validate Commit Phase isn't over
+        require(block.timestamp < commitDeadline, "Commit Phase is over");
+        
+        // If no bet was placed
+        if (bet == 0) {
+            require(msg.value > 0, "Player must set a bet with a positive amount");
+            bet == msg.value;
+            
+        // If a bet was already placed
+        } else {
+            require(msg.value == bet, "Player must match bet.");
+        }
+        
+        // If it's the Contract Deployer (Player 1)
+        if (msg.sender == players[0].addr) {
+            require(players[0].commit == 0, "You already commited");
+            assert(players[0].move == Moves.NONE); // If Player 1 hasn't yet commited, he should have no move.
+            
+            // Add Player 1's commit (his address was already set in constructor and move is already NONE by default)
+            players[0].commit = moveHash;
+        
+        // If it's not the Contract Deployer (Player 2)
+        } else {
+            require(players[1].addr == address(0), "Player 2 already exists.");
+            assert(players[1].commit == 0 && players[1].move == Moves.NONE); // If P2 doesn't exist, he should have no commit or move
+            
+            // Add Player 2
+            players[1] = Player(payable(msg.sender), moveHash, Moves.NONE);
+        }
+        
+        // As soon as both players have committed, start Reveal Phase
+        if (players[0].commit != 0 && players[1].commit != 0) {
+            
+            // Set Reveal Deadline
+            revealDeadline = block.timestamp + revealPhaseSeconds;
+        }
     }
     
+    
     // Reveal
-    function reveal(string memory move, string memory salt) external {
+    function reveal(string calldata move, string calldata salt) external {
+        
+        // Validate both players have committed
+        require(players[0].commit != 0 && players[1].commit != 0, "Can only reveal after both players commit");
+        
+        // Validate it's a valid player
+        require(msg.sender == players[0].addr || msg.sender == players[1].addr, "Only valid players can reveal.");
         
         // If the Reveal Phase is not over
-        if (now < revealDeadline) {
+        if (block.timestamp < revealDeadline) {
+            
+            // Ensure input hashes to a valid move
             require(
                 hashMatch(move, "rock") || 
                 hashMatch(move, "paper") || 
                 hashMatch(move, "scissors"), 
                 "move must be rock, paper or scissors."
             );
-            require(msg.sender == players[0].addr || msg.sender == players[1].addr, "Only valid players can reveal.");
+            
+            // Set Reveal Hash
             bytes32 revealHash = keccak256(abi.encodePacked(move, salt));
             
             // If it's Player 1
             if (msg.sender == players[0].addr) {
-                
-                // Player 2 must revealed, or at least committed
-                require(players[1].status == Status.REVEALED || players[1].status == Status.COMMITED);
                 
                 // Validate Player 1 reveal
                 validateReveal(revealHash, players[0], move);
@@ -91,22 +119,21 @@ contract RockPaperScissors {
             // If it's Player 2
             else if (msg.sender == players[1].addr) {
                 
-                // Player 1 must revealed, or at least committed
-                require(players[0].status == Status.REVEALED || players[0].status == Status.COMMITED);
-                
                 // Validate Player 2 reveal
                 validateReveal(revealHash, players[1], move);
             }
             
-            // As soon as both players have revealed, determine winner
-            if (players[0].status == Status.REVEALED && players[1].status == Status.REVEALED) {
+            // As soon as both players have revealed (have a move), determine winner
+            if (players[0].move != Moves.NONE && players[1].move != Moves.NONE) {
                 getWinner();
             }
             
-        // If the Reveal Phase is over, either player can call this function to determine winner, and get their money
+        // If the Reveal Phase is over
         } else {
-            
-            // This function is prepared for Players who don't reveal
+            /*
+                Either player can call getWinner() to get their money
+                getWinner() is prepared in case one or more players don't reveal
+            */
             getWinner();
         }
         
@@ -144,13 +171,12 @@ contract RockPaperScissors {
     
     function validateReveal(bytes32 inputHash, Player storage player, string memory _move) internal {
         
-        // To Reveal, player status must be COMMITED
-        require(player.status == Status.COMMITED, "You already revealed");
-        
+        // To Reveal, player must have not revealed before (have no move)
+        require(player.move == Moves.NONE, "You already revealed");
+    
         // If reveal is valid
         if (inputHash == player.commit) {
             player.move = strToMove(_move);
-            player.status = Status.REVEALED;
         }
         
         // If reveal is not valid
@@ -160,7 +186,7 @@ contract RockPaperScissors {
     // If no Player 2 commits until commit phase is over, Player 1 can get refund
     function refundPlayer1() external {
         require(msg.sender == players[0].addr, "Only Player 1 can get a refund");
-        require (now > commitDeadline, "You cannot get a refund until the commit phase is over");
+        require (block.timestamp > commitDeadline, "You cannot get a refund until the commit phase is over");
         require(players[1].addr == address(0), "No refunds if Player 2 exists. Wait until end of Reveal Phase");
         assert(address(this).balance == bet); // Contract balance should be equal to bet.
         players[0].addr.transfer(bet);
@@ -180,19 +206,31 @@ contract RockPaperScissors {
         if (hashMatch(input, "rock")) {return Moves.ROCK;}
         else if (hashMatch(input, "paper")) {return Moves.PAPER;}
         else if (hashMatch(input, "scissors")) {return Moves.SCISSORS;}
+        else {revert("Invalid move");}
     }
     
+    // Anyone can view Bet
     function viewBet() external view returns(uint) {
         return bet;
     }
     
+    
+    // Anyone can view time left until Commit Deadline
     function viewCommitSecondsLeft() external view returns(uint) {
-        if (now < commitDeadline) {return commitDeadline - now;}
+        
+        // Return seconds left until COmmit Deadline
+        if (block.timestamp < commitDeadline) {return commitDeadline - block.timestamp;}
         else {return 0;}
     }
     
+    // Only valid players should be able to see time left until Reveal Deadline
     function viewRevealSecondsLeft() external view returns(uint) {
-        if (now < revealDeadline) {return revealDeadline - now;}
+        
+        // Validate viewer is Player 1 or Player 2
+        require(msg.sender == players[0].addr || msg.sender == players[1].addr, "Unauthorized");
+        
+        // Return seconds left until Reveal Deadline
+        if (block.timestamp < revealDeadline) {return revealDeadline - block.timestamp;}
         else {return 0;}
     }
 }
